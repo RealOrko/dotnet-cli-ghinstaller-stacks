f30ac9775dc0b7ccf0979efcd9a10e43ca027d4c
realorko <realorko@nowhere.com>
2020-05-24 19:11:12 +0100

    Disabling nomad for now

--- install-hashicorp.fedora.32.sh
+++ install-hashicorp.fedora.32.sh
@@ -34,8 +34,8 @@ ghi build-go -d ./v1.4.2/hashicorp-vault-d6b3c72/ -o $(pwd)/vault
 ghi install -b ./vault
 
 # Install nomad
-ghi download-tag -o hashicorp -r nomad -f v0.11.2 -t
-ghi untar -t ./v0.11.2.tar -o ./v0.11.2
-ghi build-go -a get -d ./v0.11.2/hashicorp-nomad-f230806/ || true
-ghi build-go -d ./v0.11.2/hashicorp-nomad-f230806/ -o $(pwd)/nomad
-ghi install -b ./nomad
+#ghi download-tag -o hashicorp -r nomad -f v0.11.2 -t
+#ghi untar -t ./v0.11.2.tar -o ./v0.11.2
+#ghi build-go -a get -d ./v0.11.2/hashicorp-nomad-f230806/ > /dev/null || true
+#ghi build-go -d ./v0.11.2/hashicorp-nomad-f230806/ -o $(pwd)/nomad
+#ghi install -b ./nomad
